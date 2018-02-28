9eb0d093b925b8bbf6b5f7b58fbafd85584ffb17
mbuckler <markabuckler@gmail.com>
2018-02-27 20:00:00 -0500

    Add autoinstall script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Copy the .vimrc to the home directory
+cp vim-config/.vimrc ~/.vimrc
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install sensible.vim
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-sensible.git
+
+# Install all packages
+vim +PluginInstall +qall
